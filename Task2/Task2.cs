a95681157a93a9d0949fdae696adcbfa094e9154
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-22 20:09:34 +0300

    Решена задача по нахождению элементов массива, стоящих на нечетных позициях

---
+++ Task2/Task2.cs
@@ -0,0 +1,20 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+void FillArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length)
+    {
+        numbers[i] = new Random().Next(-10, 100);
+    }
+}
+
+int RecognizeOddIndex(int[] numbers, int index)
+{
+    
+}
